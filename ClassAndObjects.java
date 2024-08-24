07edca177278b2a5c57f9d3543e921953fe2acd1
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-08-24 10:45:16 +0200

    Add files via upload

---
+++ ClassAndObjects.java
@@ -0,0 +1,38 @@
+package OPPs;
+
+import java.util.Scanner;
+
+class Student {
+    // DATA MEMBERS
+    int studentId;
+    String studentName;
+    int studentAge;
+    Scanner scanner = new Scanner(System.in);
+
+
+    // MEMBER FUNCTION
+
+    public void acceptDetails(){
+        System.out.print("Enter Student Id :  ");
+        studentId = scanner.nextInt();
+        System.out.print("Enter Student Name :  ");
+        studentName = scanner.next();
+        System.out.print("Enter Student Age : ");
+        studentAge = scanner.nextInt();
+
+
+    }
+    public void displayDetails(){
+        System.out.println("Student Id : " + studentId);
+        System.out.println("Student Name : " + studentName);
+        System.out.println("Student Age : " + studentAge);
+
+
+    }
+}
+
+public class ClassAndObjects {
+    public static void main(String[] args) {
+
+    }
+}
