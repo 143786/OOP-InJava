c31f5c1663c42ef39b94ae9d201fc4fa97ab32e0
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-08-26 16:30:08 +0200

    Add files via upload

---
+++ EncapsulationInJava.java
@@ -0,0 +1,24 @@
+package OPPs;
+
+class Person{
+    private String firsName;
+    private String lastName;
+
+    public  String getFirstName(){
+       return this.firsName;
+    }
+    public void setFirsName(String firsName) {
+        this.firsName = firsName;
+
+    }
+
+
+
+}
+
+public class EncapsulationInJava {
+    public static void main(String[] args) {
+        Person person = new Person();
+
+    }
+}
