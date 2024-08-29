c5cefbbc09155a0f83cb133ab0439228673f3c17
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-08-29 15:53:31 +0200

    Add files via upload

---
+++ InheritanceInJava.java
@@ -0,0 +1,29 @@
+package OPPs;
+import java.util.Scanner;
+
+class Student {
+
+    // Data Members
+    private int studentId;
+    private String studentName;
+    private int studentAge;
+    Scanner scanner = new Scanner("System.in");
+
+    // Member Function
+    void acceptDetails(){
+        System.out.println("Enter Student Id : ");
+        studentId = scanner.nextInt();
+        System.out.println("Enter Student Name : ");
+        studentName = scanner.next();
+        System.out.println("Enter Student Age : ");
+        studentAge = scanner.nextInt();
+
+    }
+
+}
+
+
+
+
+public class InheritanceInJava {
+}
