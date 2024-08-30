40b12ea4d9286759fab230ecf1a7e760970478d0
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-08-30 15:13:53 +0200

    Add files via upload

--- UpCastingDownCastingInJava.java
+++ UpCastingDownCastingInJava.java
@@ -13,23 +13,21 @@ class Car extends Vehicle {
 
     }
     void speedUp(){
-        
+        System.out.println("Speeding up a Car");
+
     }
 
 }
 
 
-
-
-
-
-
-
-
-
-
 public class UpCastingDownCastingInJava {
     public static void main(String[] args) {
+
+        /* here in the main method, istantiating a Vehicle class, but
+       initializing the Car class. it's a Downcasting */
+
+        Vehicle vehicle = new Car();
+        
 
     }
 }
