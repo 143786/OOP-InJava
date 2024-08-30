15e0e746c420a8cdae39bc709db89ce82de2fe09
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-08-30 14:48:56 +0200

    Add files via upload

---
+++ UpCastingDownCastingInJava.java
@@ -0,0 +1,35 @@
+
+class Vehicle{
+    void drive() {
+        System.out.println("Driving a vehicle..");
+
+    }
+
+}
+
+class Car extends Vehicle {
+    void drive() {
+        System.out.println("Driving a Car.");
+
+    }
+    void speedUp(){
+        
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+public class UpCastingDownCastingInJava {
+    public static void main(String[] args) {
+
+    }
+}
