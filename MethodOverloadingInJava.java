70f9510eccf4fa0dc960397c719736ea2365bee6
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-09-02 00:46:06 +0200

    Add files via upload

--- MethodOverloadingInJava.java
+++ MethodOverloadingInJava.java
@@ -10,7 +10,18 @@ class Calculation {
 
     }
 
-    public static float addition (float num1, float num2)
+    public static float addition (float num1, float num2) {
+        return num1 + num2;
+    }
+
+    public static float addition ( int num1, float num2) {
+        return num1 + num2;
+
+    }
+    public static float addition ( float num1, int num2 ) {
+        return num1 + num2;
+
+    }
 
 
 }
@@ -18,6 +29,12 @@ class Calculation {
 
 public class MethodOverloadingInJava {
     public static void main(String[] args) {
+        System.out.println(Calculation.addition(100, 200));
+        System.out.println(Calculation.addition(100, 200, 300));
+        System.out.println(Calculation.addition(100.50F, 200.30F));
+        System.out.println(Calculation.addition(100, 200.40F));
+        System.out.println(Calculation.addition(100.39F, 200));
+
 
     }
 }
