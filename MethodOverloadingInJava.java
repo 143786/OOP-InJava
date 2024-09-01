33ad711271842e8fc1725fb8a5bc1d48de2ae010
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-09-01 15:24:46 +0200

    Add files via upload

---
+++ MethodOverloadingInJava.java
@@ -0,0 +1,23 @@
+
+
+class Calculation {
+    public static int addition(int num1, int num2) {
+        return num1 + num2;
+
+    }
+    public static int addition(int num1, int num2, int num3 ) {
+        return num1 + num2 + num3;
+
+    }
+
+    public static float addition (float num1, float num2)
+
+
+}
+
+
+public class MethodOverloadingInJava {
+    public static void main(String[] args) {
+
+    }
+}
