50ee05648d8b2cd19a5c6930fbec47c8d994013a
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-09-02 22:23:41 +0200

    Add files via upload

--- Interfaces.java
+++ Interfaces.java
@@ -2,7 +2,28 @@
 
 
 interface IBankAccount {
+    void deposit();
+    void withdraw();
+    void balance();
 
+}
+
+class Saving implements IBankAccount {
+
+    @Override
+    public void deposit() {
+        
+    }
+
+    @Override
+    public void withdraw() {
+
+    }
+
+    @Override
+    public void balance() {
+
+    }
 }
 
 
