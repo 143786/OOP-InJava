e15832f1b6d2e9a52f8f13f6d466d81c74f4112a
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-09-02 15:11:21 +0200

    Add files via upload

--- AbstractClasses.java
+++ AbstractClasses.java
@@ -4,8 +4,27 @@ abstract class BankAccount {
     abstract void withdraw();
     abstract void balance();
 
+}
 
+class SavingAccount extends BankAccount{
 
+    @Override
+    void deposit() {
+        System.out.println("Deposit in Saving Account.");
+
+    }
+
+    @Override
+    void withdraw() {
+        System.out.println("Withdraw from Saving Account.");
+
+    }
+
+    @Override
+    void balance() {
+        System.out.println("Balance in saving Account.");
+
+    }
 }
 
 
