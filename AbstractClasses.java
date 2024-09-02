56bd76719e683df732154970e3e5c88374aa66c6
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-09-02 17:05:38 +0200

    Add files via upload

--- AbstractClasses.java
+++ AbstractClasses.java
@@ -32,19 +32,19 @@ class CurrentAccount extends  BankAccount {
 
     @Override
     void deposit() {
-        System.out.println("Deposit in Saving Account.");
+        System.out.println("Deposit in Current Account.");
 
     }
 
     @Override
     void withdraw() {
-        System.out.println("Withdraw from Saving Account.");
+        System.out.println("Withdraw from Current Account.");
 
     }
 
     @Override
     void balance() {
-        System.out.println("Balance in saving Account.");
+        System.out.println("Balance in Current Account.");
 
     }
 
@@ -53,11 +53,18 @@ class CurrentAccount extends  BankAccount {
 
 public class AbstractClasses {
     public static void main(String[] args) {
+        // Object for Saving Account:
         SavingAccount savingAccount = new SavingAccount();
         savingAccount.deposit();
         savingAccount.withdraw();
         savingAccount.balance();
 
+        // Object for Current Account:
+
+        CurrentAccount currentAccount = new CurrentAccount();
+        currentAccount.deposit();
+        currentAccount.withdraw();
+        currentAccount.balance();
 
 
 
