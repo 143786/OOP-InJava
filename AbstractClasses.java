cc895751262be72d4f8e99fdd2c6356beacb3245
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-09-02 10:55:40 +0200

    Add files via upload

---
+++ AbstractClasses.java
@@ -0,0 +1,19 @@
+
+abstract class BankAccount {
+    abstract void deposit();
+    abstract void withdraw();
+    abstract void balance();
+
+
+
+}
+
+
+
+
+
+public class AbstractClasses {
+    public static void main(String[] args) {
+
+    }
+}
